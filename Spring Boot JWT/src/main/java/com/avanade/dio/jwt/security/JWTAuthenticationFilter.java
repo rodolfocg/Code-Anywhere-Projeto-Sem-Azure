471d4f223c17f7cf176feb6909324890050e014c
Rodolfo Garcia <rodolfocvgarcia@gmail.com>
2021-05-06 17:02:40 -0300

    Adding Auth0 to pom

---
+++ Spring Boot JWT/src/main/java/com/avanade/dio/jwt/security/JWTAuthenticationFilter.java
@@ -0,0 +1,33 @@
+package com.avanade.dio.jwt.security;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+
+
+
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter{
+
+    private final AuthenticationManager authenticationManager;
+
+    public JWTAuthenticationFilter(AuthenticationManager authenticationManager){
+        this.authenticationManager = authenticationManager;
+
+
+    }
+
+
+
+}
