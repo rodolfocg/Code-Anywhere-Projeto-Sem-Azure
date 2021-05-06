81dd4a5f5cfbf44402d4107f15a6d3513fa3d716
Rodolfo Garcia <rodolfocvgarcia@gmail.com>
2021-05-06 13:49:33 -0300

    User Detail Resolving Implementation

--- Spring Boot JWT/src/main/java/com/avanade/dio/jwt/SpringBootJwtApplication.java
+++ Spring Boot JWT/src/main/java/com/avanade/dio/jwt/SpringBootJwtApplication.java
@@ -3,14 +3,17 @@ package com.avanade.dio.jwt;
 import com.avanade.dio.jwt.data.UserData;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class SpringBootJwtApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootJwtApplication.class, args);
-
-        UserData user = new UserData();
-
+    }
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
--- Spring Boot JWT/src/main/java/com/avanade/dio/jwt/service/UserDetailServiceImpl.java
+++ Spring Boot JWT/src/main/java/com/avanade/dio/jwt/service/UserDetailServiceImpl.java
@@ -5,18 +5,35 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.Collections;
 
-public class UserDetailServiceImpl implements UserDetailsService {
+public abstract class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public UserDetailsServiceImpl(BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
 
     @Override
     public UserDetails LoadUserByName(String userName) throws UsernameNotFoundException{
 
         UserData user = findUser(userName);
         if (user == null) {
-            throw new UsernameNotFoundException(username);
+            throw new UsernameNotFoundException(userName);
         }
-            return new User(user.getUserName(), user.getPassword(), Collections.emptyList());
+        return new User(user.getUserName(), user.getPassword(), Collections.emptyList());
+    }
+
+
+private UserData findUser(String username) {
+
+        UserData user = new UserData();
+        user.setUserName("admin");
+        user.setPassword(bCryptPasswordEncoder.encode("ninda"));
+
+        return user;
     }
 }
