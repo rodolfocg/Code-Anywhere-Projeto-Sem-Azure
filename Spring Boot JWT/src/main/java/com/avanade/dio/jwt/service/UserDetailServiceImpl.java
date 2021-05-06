f5bcaf130d008e2c21c8a601014914bd6826d714
Rodolfo Garcia <rodolfocvgarcia@gmail.com>
2021-05-06 13:33:25 -0300

    UserDetail implementation

--- Spring Boot JWT/src/main/java/com/avanade/dio/jwt/service/UserDetailServiceImpl.java
+++ Spring Boot JWT/src/main/java/com/avanade/dio/jwt/service/UserDetailServiceImpl.java
@@ -1,4 +1,22 @@
 package com.avanade.dio.jwt.service;
 
-public class UserDetailServiceImpl {
+import com.avanade.dio.jwt.data.UserData;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Collections;
+
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    @Override
+    public UserDetails LoadUserByName(String userName) throws UsernameNotFoundException{
+
+        UserData user = findUser(userName);
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+            return new User(user.getUserName(), user.getPassword(), Collections.emptyList());
+    }
 }
